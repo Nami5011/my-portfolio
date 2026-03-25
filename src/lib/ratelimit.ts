d5c7982c8e1fb373d7c2267de9ea47c1d5156d4c
NamiSawada <maron.011821@gmail.com>
2026-03-25 17:07:30 +0100

    feat: add ratelimit

---
+++ src/lib/ratelimit.ts
@@ -0,0 +1,14 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+
+type Unit = 'ms' | 's' | 'm' | 'h' | 'd';
+type Duration = `${number} ${Unit}` | `${number}${Unit}`;
+
+export function createRateLimiter(requests: number, duration: Duration, path: string) {
+  return new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(requests, duration),
+    analytics: true,
+    prefix: `@ratelimit/${path}`,
+  });
+}
